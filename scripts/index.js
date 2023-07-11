0c64ab8ffd85a035ccbffe6119327ca5f5f9c063
AnnZblt <ann.zblt@yandex.ru>
2023-07-11 19:17:46 +0300

    Add interactive with burger-menu and accordion

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
 import beginTimer from '/scripts/modules/timer.js';
+import accordion from '/scripts/modules/accordion.js';
+import burger from '/scripts/modules/burger.js';
 
 beginTimer();
+accordion();
+burger();
---
+++ scripts/modules/burger.js
@@ -0,0 +1,16 @@
+const burger = () => {
+  const headerContainer = document.querySelector('.header__container');
+  const burgerButton = document.querySelector('.header__menu-button');
+  const burgerMenu = document.querySelector('.header__menu');
+
+  headerContainer.addEventListener('click', (event) => {
+    console.log(event.target);
+    if (event.target === burgerButton) {
+      burgerMenu.classList.toggle('header__menu_active');
+    } else {
+      burgerMenu.classList.remove('header__menu_active');
+    }
+  });
+};
+
+export default burger;
