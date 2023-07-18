b5a255011bd9ee07615dc8455029ab15efd1d7b3
AnnZblt <ann.zblt@yandex.ru>
2023-07-18 15:10:50 +0300

    Add edits by HW + 1 bonus task

--- scripts/modules/timer.js
+++ scripts/modules/timer.js
@@ -1,7 +1,14 @@
 const beginTimer = () => {
-  const nominativeUnits = [1, 21, 31, 41, 51];
-  const genitiveUnits =
-    [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54];
+  const daysDeclension = ['день', 'дня', 'дней'];
+  const hoursDeclension = ['час', 'часа', 'часов'];
+  const minutesDeclension = ['минута', 'минуты', 'минут'];
+  const secondsDeclension = ['секунда', 'секунды', 'секунд'];
+  const msPerSec = 1000;
+  const secPerMin = 60;
+  const minPerHour = 60;
+  const hoursPerDay = 24;
+  const oneDayRemaining = hoursPerDay * minPerHour * secPerMin * msPerSec;
+  const GMTplusThree = 3 * minPerHour;
 
   const timerDays = document.querySelector('.timer__count_days');
   const timerHours = document.querySelector('.timer__count_hours');
@@ -13,70 +20,81 @@ const beginTimer = () => {
     document.querySelector('.timer').getAttribute('data-timer-deadline');
   const promotionText = document.querySelector('.hero__text');
   const promotionTimer = document.querySelector('.hero__timer');
+  const timerDaysContainer = document.querySelector('.timer__item_days');
+
+  const convertDateToGMT = (date, offset) => {
+    const utc =
+      date.getTime() + (date.getTimezoneOffset() * (msPerSec * secPerMin));
+    const newDate = new Date(utc + (msPerSec * secPerMin * offset));
+    return newDate;
+  };
 
   const getTimeRemaining = () => {
+    const dateNow = convertDateToGMT(new Date(), GMTplusThree);
     const dateStop = new Date(deadline).getTime();
-    const dateNow = Date.now();
     const timeRemaining = dateStop - dateNow;
 
+    const seconds = Math.floor(timeRemaining / 1000 % 60);
     const minutes = Math.floor(timeRemaining / 1000 / 60 % 60);
     const hours = Math.floor(timeRemaining / 1000 / 60 / 60 % 24);
     const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
 
     return {
       timeRemaining,
+      seconds,
       minutes,
       hours,
       days,
     };
   };
 
+  const wordDeclension = (num, arr) => {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return arr[(num % 100 > 4 && num % 100 < 20) ?
+      2 : cases[(num % 10 < 5) ? num % 10 : 5]];
+  };
+
+  const numsTwoDigit = (num) => `0${num}`.slice(-2);
+
+  const oneDayRemainingTimer = () => {
+    const timer = getTimeRemaining();
+    if (timer.timeRemaining < oneDayRemaining) {
+      timerDaysContainer.remove();
+      promotionTimer.insertAdjacentHTML('beforeend', `
+      <p class="timer__item timer__item_seconds">
+        <span class="timer__count timer__count_seconds">00</span>
+        <span class="timer__units timer__units_seconds">секунд</span>
+      </p>
+    `);
+    }
+  };
+
+  oneDayRemainingTimer();
+
   const startTimer = () => {
     const timer = getTimeRemaining();
 
     timerDays.textContent = timer.days;
-    timerHours.textContent = timer.hours;
-    timerMinutes.textContent = timer.minutes;
+    timerHours.textContent = numsTwoDigit(timer.hours);
+    timerMinutes.textContent = numsTwoDigit(timer.minutes);
 
     const intervalId = setTimeout(startTimer, 1000);
 
+    const daysText = wordDeclension(timer.days, daysDeclension);
+    const hoursText = wordDeclension(timer.hours, hoursDeclension);
+    const minutesText = wordDeclension(timer.minutes, minutesDeclension);
 
-    if (timer.hours < 10) {
-      timerHours.textContent = '0' + timer.hours;
-    }
+    daysUnits.textContent = daysText;
+    hoursUnits.textContent = hoursText;
+    minutesUnits.textContent = minutesText;
 
-    if (timer.minutes < 10) {
-      timerMinutes.textContent = '0' + timer.minutes;
-    }
+    if (timer.timeRemaining < oneDayRemaining) {
+      const timerSeconds = document.querySelector('.timer__count_seconds');
+      const secondsUnits = document.querySelector('.timer__units_seconds');
 
-    if (nominativeUnits.includes(timer.days)) {
-      daysUnits.textContent = 'день';
-    } else {
-      daysUnits.textContent = 'дней';
-    }
-
-    if (nominativeUnits.includes(timer.hours)) {
-      hoursUnits.textContent = 'час';
-    } else {
-      hoursUnits.textContent = 'часов';
-    }
-
-    if (nominativeUnits.includes(timer.minutes)) {
-      minutesUnits.textContent = 'минута';
-    } else {
-      minutesUnits.textContent = 'минут';
-    }
-
-    if (genitiveUnits.includes(timer.days)) {
-      daysUnits.textContent = 'дня';
-    }
-
-    if (genitiveUnits.includes(timer.hours)) {
-      hoursUnits.textContent = 'часа';
-    }
-
-    if (genitiveUnits.includes(timer.minutes)) {
-      minutesUnits.textContent = 'минуты';
+      timerSeconds.textContent = numsTwoDigit(timer.seconds);
+      const secondsText = wordDeclension(timer.seconds, secondsDeclension);
+      secondsUnits.textContent = secondsText;
     }
 
     if (timer.timeRemaining <= 0) {
