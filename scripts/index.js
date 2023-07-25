da98211e839e857bd7e0bb72e2bd9ad3f904f2f9
AnnZblt <ann.zblt@yandex.ru>
2023-07-25 13:25:52 +0300

    Add airplane animation. Add burger show/hide animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import beginTimer from '/scripts/modules/timer.js';
 import accordion from '/scripts/modules/accordion.js';
 import burger from '/scripts/modules/burger.js';
+import '/scripts/modules/airplane.js';
 
 beginTimer();
 accordion();
