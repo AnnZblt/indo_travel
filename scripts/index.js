c6ba80897b8446eb8a9d75832818b56befdba3e6
AnnZblt <ann.zblt@yandex.ru>
2023-08-01 14:48:36 +0300

    Add reservation DB and drop-down menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ import beginTimer from '/scripts/modules/timer.js';
 import accordion from '/scripts/modules/accordion.js';
 import burger from '/scripts/modules/burger.js';
 import '/scripts/modules/airplane.js';
+import '/scripts/modules/tourPrice.js';
+
 
 beginTimer();
 accordion();
